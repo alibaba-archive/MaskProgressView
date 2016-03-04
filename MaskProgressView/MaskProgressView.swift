73b85bc687e30eba62c9c044520f5c0fda9c7849
Xin Hong <hongxinhope@gmail.com>
2016-03-04 13:09:20 +0800

    Initial Commit

---
+++ MaskProgressView/MaskProgressView.swift
@@ -0,0 +1,13 @@
+//
+//  MaskProgressView.swift
+//  MaskProgressView
+//
+//  Created by Xin Hong on 16/3/4.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class MaskProgressView: UIView {
+
+}
---
+++ MaskProgressViewExample/ExampleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleViewController.swift
+//  MaskProgressViewExample
+//
+//  Created by 洪鑫 on 16/3/4.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
